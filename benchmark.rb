86b5be32fa6fbc9b8034ceb56d8921097ea3bfab
David Albert <davidbalbert@gmail.com>
2014-08-10 23:04:24 -0400

    Added benchmark with pathological case.

---
+++ benchmark.rb
@@ -0,0 +1,19 @@
+$LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
+
+require 're3'
+
+require 'benchmark'
+
+
+1.upto(30) do |n|
+  regexp_string = "a?" * n + "a" * n
+  input = "a" * n
+
+  puts
+  puts "n = #{n}"
+
+  Benchmark.bm do |x|
+    x.report("Ruby:") { Regexp.new(regexp_string).match input }
+    x.report("Re3:") { Re3::Regexp.new(regexp_string).match input }
+  end
+end
