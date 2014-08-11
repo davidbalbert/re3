9895ea4bbe306374a7699c2004ca08447a3e9a12
David Albert <davidbalbert@gmail.com>
2014-08-10 20:45:05 -0400

    AST -> NFA compiler

--- lib/re3.rb
+++ lib/re3.rb
@@ -1,14 +1,56 @@
 require "re3/version"
 require 're3/parser'
+require 're3/nfa'
 
 module Re3
   class Regexp
     def initialize(str)
-      @states = Compiler.new(str).compile
-
+      @nodes = Parser.new.parse(str)
+      @states = Compiler.new(@nodes).compile
     end
 
     def match(s, engine = :nfa)
     end
+  end
+
+  class Compiler
+    include NFA
+
+    def initialize(nodes)
+      @nodes = nodes
+    end
+
+    def compile
+      walk_and_compile(@nodes, AcceptState.new)
+    end
+
+    private
+    def walk_and_compile(nodes, next_state)
+      case nodes[0]
+      when :char
+        State.new(nodes[1], next_state)
+      when :and
+        right = walk_and_compile(nodes[2], next_state)
+        walk_and_compile(nodes[1], right)
+      when :or
+        left = walk_and_compile(nodes[1], next_state)
+        right = walk_and_compile(nodes[2], next_state)
+        SplitState.new(left, right)
+      when :maybe
+        SplitState.new(walk_and_compile(nodes[1], next_state), next_state)
+      when :any
+        res = SplitState.new(nil, next_state)
+        res.loop_left(walk_and_compile(nodes[1], res))
+
+        res
+      when :one_plus
+        split = SplitState.new(nil, next_state)
+        res = walk_and_compile(nodes[1], split)
+        split.loop_left(res)
+
+        res
+      end
+    end
+
   end
 end
---
+++ lib/re3/nfa.rb
@@ -0,0 +1,27 @@
+module Re3
+  module NFA
+    class AcceptState; end
+
+    class State
+      attr_reader :char, :next_state
+
+      def initialize(char, next_state)
+        @char = char
+        @next_state  = next_state
+      end
+    end
+
+    class SplitState
+      attr_reader :left, :right
+
+      def initialize(left, right)
+        @left  = left
+        @right = right
+      end
+
+      def loop_left(new_left)
+        @left = new_left
+      end
+    end
+  end
+end
