9799ee4cd4ac1a498f1320d9002b27c2f3aa71d7
David Albert <davidbalbert@gmail.com>
2014-08-17 18:26:22 -0400

    The recrusive engine is most basic. Put it first.

--- lib/re3/engines.rb
+++ lib/re3/engines.rb
@@ -1,28 +1,5 @@
 module Re3
   module Engines
-    class ThompsonEngine
-      def initialize(regexp, input)
-        @input  = input
-        @current_states = regexp.start_state.expand.to_set
-      end
-
-      def match
-        @input.each_char do |c|
-          step(c)
-        end
-
-        @current_states.any?(&:accepts?)
-      end
-
-      private
-
-      def step(c)
-        @current_states = @current_states.map do |state|
-          state.next_states_for(c)
-        end.flatten.to_set
-      end
-    end
-
     class RecursiveEngine
       include States
 
@@ -54,6 +31,29 @@ module Re3
             false
           end
         end
+      end
+    end
+
+    class ThompsonEngine
+      def initialize(regexp, input)
+        @input  = input
+        @current_states = regexp.start_state.expand.to_set
+      end
+
+      def match
+        @input.each_char do |c|
+          step(c)
+        end
+
+        @current_states.any?(&:accepts?)
+      end
+
+      private
+
+      def step(c)
+        @current_states = @current_states.map do |state|
+          state.next_states_for(c)
+        end.flatten.to_set
       end
     end
   end
