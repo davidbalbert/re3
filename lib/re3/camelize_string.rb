23aa7c849365d886dfe90dd32be8a4bd06213ed2
David Albert <davidbalbert@gmail.com>
2014-08-17 22:19:10 -0400

    Properly nest CamelizeString

--- lib/re3/camelize_string.rb
+++ lib/re3/camelize_string.rb
@@ -1,7 +1,9 @@
-module CamelizeString
-  refine String do
-    def camelize
-      split("_").map(&:capitalize).join
+module Re3
+  module CamelizeString
+    refine String do
+      def camelize
+        split("_").map(&:capitalize).join
+      end
     end
   end
 end
